bdfe241cd33041e108cb1d3cdcba6de4afbfcddf
Kristian Glass <git@doismellburning.co.uk>
2015-05-27 19:45:40 +0100

    Actual testing!
    I'm pleasantly surprised, I didn't expect my nasty direct bash-to-python
    conversion to be so easy to run (even if the tests aren't particularly
    high-quality)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import boto
+import mock
 import moto
 import unittest
 
@@ -6,11 +7,33 @@ import unittest
 class CLITests(unittest.TestCase):
 
     @moto.mock_s3
-    def test_deploy(self):
+    @mock.patch('boto.beanstalk.layer1.Layer1.create_application_version')
+    @mock.patch('boto.beanstalk.layer1.Layer1.update_environment')
+    def test_deploy(self, cav, ue):
         s3 = boto.connect_s3()
         s3.create_bucket("laterpay-rubberjack-ebdeploy")  # FIXME Remove hardcoded bucket name
 
         import rubberjackcli.deploy  # noqa
 
-    def test_promote(self):
-        pass
+    @moto.mock_s3
+    @mock.patch('boto.beanstalk.layer1.Layer1.describe_environments')
+    @mock.patch('boto.beanstalk.layer1.Layer1.update_environment')
+    def test_promote(self, ue, de):
+        de.return_value = {
+            'DescribeEnvironmentsResponse': {
+                'DescribeEnvironmentsResult': {
+                    'Environments': [
+                        {
+                            'EnvironmentName': 'laterpay-devnull-live',  # FIXME Remove hardcoded EnvName
+                            'VersionLabel': 'old',
+                        },
+                        {
+                            'EnvironmentName': 'laterpay-devnull-dev',  # FIXME Remove hardcoded EnvName
+                            'VersionLabel': 'new',
+                        },
+                    ],
+                },
+            },
+        }
+
+        import rubberjackcli.promote  # noqa
