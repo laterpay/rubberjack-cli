f98a4c076b178454d6cfe0dcc8508c460d998477
Kristian Glass <git@doismellburning.co.uk>
2015-05-27 19:06:07 +0100

    It helps if I use moto to _create_ a bucket before accessing it
    Thanks to @oddbloke for pointing this out!

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import boto
 import moto
 import unittest
 
@@ -6,7 +7,10 @@ class CLITests(unittest.TestCase):
 
     @moto.mock_s3
     def test_deploy(self):
-        import rubberjackcli.deploy
+        s3 = boto.connect_s3()
+        s3.create_bucket("laterpay-rubberjack-ebdeploy")  # FIXME Remove hardcoded bucket name
+
+        import rubberjackcli.deploy  # noqa
 
     def test_promote(self):
         pass
